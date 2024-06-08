eb0ca1c04f076f00e2406c43df530c51c01d3ad4
Dochanii <dmlwhd010@naver.com>
2024-06-08 17:17:26 +0900

    [FEAT] 레벨 5가 되면 레벨업 ui 띄우게 설정, 레벨업  ui에 받은 이미지 2개로 5랩마다 랜덤으로 아이템 이미지 뿌리기 기능 구현

---
+++ src/LevelUpUI.py
@@ -0,0 +1,54 @@
+import pygame
+import random
+from Button import Button
+
+class LevelUpUI:
+    def __init__(self, screen, level_up_image_path, game_instance):
+        self.screen = screen
+        self.level_up_image = pygame.image.load(level_up_image_path)
+        self.rect = self.level_up_image.get_rect(center=(650, 400))
+        self.active = False
+        self.game_instance = game_instance
+
+        self.button_width = 250
+        self.button_height = 70
+        self.item_button_x = self.rect.centerx - self.button_width // 2
+        self.item_button_y = self.rect.centery - (self.button_height // 2) - 25
+        # 아이템 추가 넣는 곳
+        self.item_images = ["./image/carrot.png", "./image/appleItem.png"]
+
+        # 초기 버튼 설정; 실제 사용은 activate()에서
+        self.initialize_buttons()
+
+    def initialize_buttons(self):
+        # 랜덤 아이템 선택을 위해 매번 새로운 Button 인스턴스 생성
+        self.item_button = Button(
+            self.item_button_x, self.item_button_y, self.button_width, self.button_height,
+            self.resume_game, "./image/SelectItemSlot.png", self.item_images
+        )
+        # 상태 아이템 추가 넣는 곳
+        self.status_images = []
+        self.status_button = Button(
+            self.item_button_x, self.item_button_y + self.button_height + 20, self.button_width, self.button_height,
+            self.resume_game, "./image/SelectItemSlot.png", []
+        )
+        
+        self.buttons = [self.item_button, self.status_button]
+
+    def draw(self):
+        if self.active:
+            self.screen.blit(self.level_up_image, self.rect.topleft)
+            for button in self.buttons:
+                button.draw(self.screen)
+
+    def handle_event(self, event):
+        for button in self.buttons:
+            button.handle_event(event)
+
+    def resume_game(self):
+        self.active = False
+        self.game_instance.paused = False
+
+    def activate(self):
+        self.initialize_buttons()  # 버튼을 재초기화하여 랜덤 이미지를 반영
+        self.active = True
