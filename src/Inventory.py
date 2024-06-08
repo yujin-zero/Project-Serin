26ed45d57943c0d1ed00036a47ba9b24f0395929
Huro0906 <huro0906@gmail.com>
2024-06-09 07:17:59 +0900

    Feat : Weapon이 inventory에서 작동하도록 변경

--- src/Inventory.py
+++ src/Inventory.py
@@ -1,9 +1,18 @@
+from LeafWeapon import LeafWeapon;
 
 
 class Inventory:
-    def __init__(self): 
+    def __init__(self, Serin, Screen, weapon_sprite): 
         self.item_list = list()
+        self.leaf_weapon = LeafWeapon(Serin, Screen, weapon_sprite)
+        self.add_item(self.leaf_weapon);
 
-    def add_item(self):
-        self.item_list.append()
+    def add_item(self, item):
+        self.item_list.append(item)
+
+    def attck(self):
+        for item in self.item_list:
+            print(item)
+            item.attack()
+    
 
--- src/leafWeapon.py
+++ src/leafWeapon.py
@@ -13,7 +13,7 @@ class Leaf(pygame.sprite.Sprite):
         pygame.draw.polygon(self.image, GREEN, [(5, 0), (10, 5), (5, 10), (0, 5)])  # Simple leaf shape
         self.rect = self.image.get_rect(center=(x, y))
         self.angle = angle
-        self.speed = 5
+        self.speed = 8
         self.creation_time = pygame.time.get_ticks()
 
 
@@ -25,24 +25,32 @@ class Leaf(pygame.sprite.Sprite):
         if pygame.time.get_ticks() - self.creation_time > 2000:
             self.kill()
 
+    def draw(self, screen, camera_x, camera_y):
+        screen.blit(self.image, (self.rect.x -
+                    camera_x, self.rect.y - camera_y))
+
+
 # Leaf class
 class LeafWeapon:
-    def __init__(self, serin, screen):
+    def __init__(self, serin, screen, all_sprite):
         self.serin = serin
         self.screen = screen
         self.last_shot = pygame.time.get_ticks()
-        self.shoot_delay = {1000,900,900,800,700}
-        self.angle = {180,180,90,90,45}
-        self.level = 0;
+        self.shoot_delay = [0,1000,900,900,800,700]
+        self.angle = [0,180,180,90,90,45]
+        self.damage = [0,10,15,15,20,25]
+        self.level = 3;
         self.maxLevel = 5;
+        self.all_sprite = all_sprite;
 
-    def attack(self, all_sprites, leaves):
+    def attack(self):
+        if self.level == 0 :
+            return
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay[self.level]:
             self.last_shot = now
             for angle in range(0, 360, self.angle[self.level]):  
                 rad_angle = math.radians(angle)
                 leaf = Leaf(self.serin.rect.centerx, self.serin.rect.centery, rad_angle)
-                all_sprites.add(leaf)
-                leaves.add(leaf)
+                self.all_sprite.add(leaf)
         
