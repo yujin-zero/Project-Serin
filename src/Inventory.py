43fe3d099ac2d15733bc0c829699894d22ca42a6
Huro0906 <huro0906@gmail.com>
2024-06-09 11:53:36 +0900

    Feat : leaf 이미지 추가 및 몬스터 충돌 이벤트 추가

--- src/Inventory.py
+++ src/Inventory.py
@@ -1,18 +1,35 @@
-from LeafWeapon import LeafWeapon;
-
+from LeafWeapon import LeafWeapon
+from WingBoots import WingBoots
+from Heart import Heart
 
 class Inventory:
     def __init__(self, Serin, Screen, weapon_sprite): 
+        self.weapon_list = list()
         self.item_list = list()
         self.leaf_weapon = LeafWeapon(Serin, Screen, weapon_sprite)
-        self.add_item(self.leaf_weapon);
+        self.add_weapon(self.leaf_weapon);
+        self.serin = Serin
+        self.wing_boots = WingBoots()
+        self.add_item(self.wing_boots);
+        self.heart = Heart()
+
+
+    def add_weapon(self, item):
+        self.weapon_list.append(item)
 
     def add_item(self, item):
         self.item_list.append(item)
 
-    def attck(self):
+    def update_item(self):
         for item in self.item_list:
-            print(item)
+            item.update(self.serin)
+
+    def attck(self):
+        for item in self.weapon_list:
             item.attack()
+
+    def heal(self):
+        self.heart.update(self.serin)
+
     
 
--- src/leafWeapon.py
+++ src/leafWeapon.py
@@ -2,18 +2,15 @@ import pygame
 import math
 import random
 
-# Colors
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-
 class Leaf(pygame.sprite.Sprite):
-    def __init__(self, x, y, angle):
+    def __init__(self, x, y, angle, damage):
         super().__init__()
-        self.image = pygame.Surface((10, 10), pygame.SRCALPHA)
-        pygame.draw.polygon(self.image, GREEN, [(5, 0), (10, 5), (5, 10), (0, 5)])  # Simple leaf shape
+        self.image =pygame.image.load("./image/leaf.png").convert_alpha()
+
         self.rect = self.image.get_rect(center=(x, y))
         self.angle = angle
-        self.speed = 8
+        self.speed = 14
+        self.damage = damage
         self.creation_time = pygame.time.get_ticks()
 
 
@@ -24,13 +21,14 @@ class Leaf(pygame.sprite.Sprite):
         #2초가 지나면 삭제
         if pygame.time.get_ticks() - self.creation_time > 2000:
             self.kill()
+        self.image = pygame.transform.rotate(self.image, -math.degrees(0.05))
+        self.rect = self.image.get_rect(center=self.rect.center)
 
     def draw(self, screen, camera_x, camera_y):
         screen.blit(self.image, (self.rect.x -
                     camera_x, self.rect.y - camera_y))
 
 
-# Leaf class
 class LeafWeapon:
     def __init__(self, serin, screen, all_sprite):
         self.serin = serin
@@ -39,7 +37,7 @@ class LeafWeapon:
         self.shoot_delay = [0,1000,900,900,800,700]
         self.angle = [0,180,180,90,90,45]
         self.damage = [0,10,15,15,20,25]
-        self.level = 3;
+        self.level = 5;
         self.maxLevel = 5;
         self.all_sprite = all_sprite;
 
@@ -51,6 +49,6 @@ class LeafWeapon:
             self.last_shot = now
             for angle in range(0, 360, self.angle[self.level]):  
                 rad_angle = math.radians(angle)
-                leaf = Leaf(self.serin.rect.centerx, self.serin.rect.centery, rad_angle)
+                leaf = Leaf(self.serin.rect.centerx, self.serin.rect.centery, rad_angle, self.damage[self.level])
                 self.all_sprite.add(leaf)
         
